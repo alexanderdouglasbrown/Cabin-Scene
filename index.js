6c172708655c49d9512bfd2b5d2bacb093728538
Alex Brown <alexanderdouglasbrown@gmail.com>
2023-06-16 18:00:38 -0700

    Basic dot on screen. Handle resize.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import('./pkg').catch(console.error)
 
 // import objLoader from './src/objLoader'
-import createShader from './src/createShader'
-import createProgram from './src/createProgram'
+import { createShader, createProgram } from './src/shaderFunctions'
 
 const vertexShaderGLSL = `#version 300 es
 
 in vec4 a_position;
 
 void main() {
-    gl_Position = a_position;
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_PointSize = 15.0;
+    // gl_Position = a_position;
 }
 `
 
@@ -19,13 +20,22 @@ precision highp float;
 out vec4 outColor;
 
 void main() {
-    outColor = vec4(1, 1, 1, 1);
+    outColor = vec4(1, 0, 1, 1);
 }
 `
+
+const handleResize = (gl) => {
+    gl.canvas.width = gl.canvas.clientWidth
+    gl.canvas.height = gl.canvas.clientHeight
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+}
 
 const main = async () => {
     const canvas = document.querySelector("#canvas")
     const gl = canvas.getContext("webgl2")
+
+    handleResize(gl)
 
     if (!gl) {
         console.error("WebGL failed to load")
@@ -36,7 +46,14 @@ const main = async () => {
     const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderGLSL)
     const program = createProgram(gl, vertexShader, fragmentShader)
 
+    gl.useProgram(program)
+
     const draw = () => {
+        if (canvas.clientWidth !== canvas.width || canvas.clientHeight !== canvas.height)
+            handleResize(gl)
+
+        gl.drawArrays(gl.POINTS, 0, 1)
+
         requestAnimationFrame(draw)
     }
 
--- src/shaderFunctions.js
+++ src/shaderFunctions.js
@@ -15,4 +15,20 @@ const createProgram = (gl, vertexShader, fragmentShader) => {
     }
 }
 
-export default createProgram
+const createShader = (gl, type, source) => {
+    const shader = gl.createShader(type)
+
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+
+    if (success) {
+        return shader
+    } else {
+        console.error(gl.getShaderInfoLog(shader))
+        gl.deleteShader(shader)
+    }
+}
+
+export { createProgram, createShader }
