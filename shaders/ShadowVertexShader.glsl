1e20bf9def67e6d55fc8e2a9cb9a5872dccb679a
Alex Brown <alexanderdouglasbrown@gmail.com>
2024-12-06 23:16:52 -0800

    Repair world and view matrixes

--- shaders/ShadowVertexShader.glsl
+++ shaders/ShadowVertexShader.glsl
@@ -5,8 +5,7 @@ in vec4 a_position;
 uniform mat4 u_projection;
 uniform mat4 u_view;
 uniform mat4 u_world;
-uniform mat4 u_model;
 
 void main() {
-    gl_Position = u_projection * u_view * u_world * u_model * a_position;
+    gl_Position = u_projection * u_view * u_world * a_position;
 }
--- shaders/SunVertexShader.glsl
+++ shaders/SunVertexShader.glsl
@@ -3,7 +3,6 @@
 in vec4 a_position;
 in vec2 a_textureCoord;
 
-uniform mat4 u_model;
 uniform mat4 u_world;
 uniform mat4 u_view;
 uniform mat4 u_projection;
@@ -11,7 +10,7 @@ uniform mat4 u_projection;
 out vec2 v_textureCoord;
 
 void main() {
-    gl_Position = u_projection * u_view * u_world * u_model * a_position;
+    gl_Position = u_projection * u_view * u_world * a_position;
 
     v_textureCoord = a_textureCoord;
 }
